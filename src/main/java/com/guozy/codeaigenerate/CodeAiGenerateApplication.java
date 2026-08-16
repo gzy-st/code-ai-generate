5f11bf1e1076fe6b5b09e62ee24a43c6af79d9dd
gzy-st <guozy>
2026-08-16 15:53:31 +0800

    用户管理

--- src/main/java/com/guozy/codeaigenerate/CodeAiGenerateApplication.java
+++ src/main/java/com/guozy/codeaigenerate/CodeAiGenerateApplication.java
@@ -1,11 +1,13 @@
 package com.guozy.codeaigenerate;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @EnableAspectJAutoProxy(exposeProxy = true)
 @SpringBootApplication
+@MapperScan("com.guozy.codeaigenerate.mapper")
 public class CodeAiGenerateApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/guozy/codeaigenerate/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.guozy.codeaigenerate.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.guozy.codeaigenerate.model.entity.User;
+
+/**
+ *  映射层。
+ *
+ * @author <a href="https://github.com/gzy-st">程序员郭志勇</a>
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
