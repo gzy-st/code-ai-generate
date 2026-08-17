c9354c466f85309d6b720248d702a94dc662911d
gzy-st <guozy>
2026-08-17 21:10:44 +0800

    feature_流式输出

--- src/main/java/com/guozy/codeaigenerate/ai/AiCodeGeneratorService.java
+++ src/main/java/com/guozy/codeaigenerate/ai/AiCodeGeneratorService.java
@@ -3,6 +3,7 @@ package com.guozy.codeaigenerate.ai;
 import com.guozy.codeaigenerate.ai.model.HtmlCodeResult;
 import com.guozy.codeaigenerate.ai.model.MultiFileCodeResult;
 import dev.langchain4j.service.SystemMessage;
+import reactor.core.publisher.Flux;
 
 public interface AiCodeGeneratorService {
     /**
@@ -10,13 +11,32 @@ public interface AiCodeGeneratorService {
      * @param userMessage
      * @return
      */
-    @SystemMessage(fromResource = "prompt/singlefile.text")
+    @SystemMessage(fromResource = "prompt/singlefile.txt")
     HtmlCodeResult generateCodeWithSingleFile(String userMessage);
     /**
      * 生成多文件
      * @param userMessage
      * @return
      */
-    @SystemMessage(fromResource = "prompt/morefile.text")
+    @SystemMessage(fromResource = "prompt/morefile.txt")
     MultiFileCodeResult generateCodeWithMultipleFiles(String userMessage);
+
+    /**
+     * 生成 HTML 代码（流式）
+     *
+     * @param userMessage 用户消息
+     * @return 生成的代码结果
+     */
+    @SystemMessage(fromResource = "prompt/singlefile.txt")
+    Flux<String> generateHtmlCodeStream(String userMessage);
+
+    /**
+     * 生成多文件代码（流式）
+     *
+     * @param userMessage 用户消息
+     * @return 生成的代码结果
+     */
+    @SystemMessage(fromResource = "prompt/morefile.txt")
+    Flux<String> generateMultiFileCodeStream(String userMessage);
+
 }
